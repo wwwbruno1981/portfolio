b1fae537c6a6ffc0d7a878b0cbe88e99cbfb0174
wwwbruno1981 <wwwbrruno@gmail.com>
2021-09-10 13:20:07 -0300

    atualicao

--- components/footer.js
+++ components/footer.js
@@ -1,15 +1,11 @@
-function Outdoor() {
+function Footer() {
     return (
 
 <footer className="shadow-2xl bg-gradient-to-r from-red-700 via-red-900 to-red-700 flex items-center justify-center w-full h-24 border-t">
-<a
-  className="text-white flex items-center justify-center"
-
->
-  Todos os direitos reservados.{' '}
- 
-</a>
+ <a className="text-white flex items-center justify-center">
+   Todos os direitos reservados.{' '}
+ </a>
 </footer>
     )
 }
-export default Outdoor
+export default Footer
---
+++ pages/site.js
@@ -0,0 +1,14 @@
+import Nav from '../components/nav'
+import Footer from '../components/footer'
+
+function Site() {
+    return (
+       <>
+        <Nav />  
+
+
+        <Footer />
+      </>
+    )
+}
+export default Site
